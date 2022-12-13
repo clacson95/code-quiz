0374cc72fce5a139062b0f7f0a442466faf58779
clacson95 <caleb.lacson@gmail.com>
2022-12-13 18:37:28 -0500

    added questions and js files

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+// quiz questions
+const questions = [
+    {
+        question: "Which of the following keywords is used to define a variable in JavaScript?",
+        choices: ["a. var", "b. let", "c. Both a and b", "d. None of the above"],
+        answer: "c. Both a and b"
+    },
+    {
+        question: "Which built-in method returns the calling string value converted to upper case?",
+        choices: ["a. toUpperCase()", "b. toUpper()", "c. changeCase(case)", "d. parentheses"],
+        answer: "c. quotes"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "b. other arrays"
+    },
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b. =="
+    },
+    {
+        question: "The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a. 0"
+    },
+    {
+        question: "Which of the following is not a JavaScript framework?",
+        choices: ["a. Node", "b. Vue", "c. React", "d. Cassandra"],
+        answer: "d. Cassandra"
+    },
+];
+
